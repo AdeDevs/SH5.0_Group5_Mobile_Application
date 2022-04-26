a5eca8741fdcabcbb446ca2cabd04b047998e01b
AdeDevs <89089354+AdeDevs@users.noreply.github.com>
2022-04-26 12:13:55 +0100

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 //This initializes the app
 void main() {
-  
   //These request input from user
   stdout.write("Enter Your Subject: ");
   String subject = (stdin.readLineSync()!);
@@ -14,14 +13,12 @@ void main() {
   //These are terms that determine grade of student
   if (Score >= 80) {
     print("Your Grade For $subject is A");
-  } else if (Score >= 70) {
+  } else if (Score >= 60 && Score <= 79) {
     print("Your Grade For $subject is B");
-  } else if (Score >= 50) {
+  } else if (Score >= 50 && Score <= 59) {
     print("Your Grade For $subject is C");
-  } else if (Score >= 40) {
+  } else if (Score >= 40 && Score <= 49) {
     print("Your Grade For $subject is D");
-  } else if (Score >= 20) {
-    print("Your Grade For $subject is E");
   } else {
     print("Your Grade For $subject is F");
   }
